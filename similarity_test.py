23b8098148532c32682abd6c51d910e54365870a
yoowonsuk <yoowonsuk@github.com>
2020-05-18 21:36:10 +0900

    similarity_test.py

--- similarity_test.py
+++ similarity_test.py
@@ -3,8 +3,12 @@ with open('text8', 'r') as f:
     text = f.read()
 # Write your code of data processing, training, and evaluation
 # Full training takes very long time. We recommend using a subset of text8 when you debug
-_, word2id, id2word = preprocess(text, subset=1e-4)
-w_emb = ##### pickle load #####
+#_, word2id, id2word = preprocess(text, subset=1e-4)
+with open('cbow_params.pkl', 'rb') as f:
+    w_emb, w_out, word2id, id2word = pickle.load(f)
+close(f)
+
+
 ################## similarity test ##################
 most_similar_byEmb(word, word2id, id2word, w_emb, top=5)
 # example #
