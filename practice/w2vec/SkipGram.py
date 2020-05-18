0556047e1cd4cc575e4753c95da74246d3990424
unknown <sodlqnf123@naver.com>
2020-05-18 11:45:16 +0900

    last commit

--- practice/w2vec/SkipGram.py
+++
@@ -1,4 +0,0 @@
-import sys
-sys.path.append('..')
-from common.model import Net
-from common.layer import ParellelOutputAffine, Affine, Sigmoid, SoftmaxWithLoss
--- word2vec.py
+++ word2vec.py
@@ -1,101 +1,51 @@
 import torch
-from random import shuffle
+import random
 from collections import Counter
 import argparse
 import torch.nn as nn # use plan embeding 
+import sys
+sys.path.append('..')
+from common.optimizer import SGD
+from common.util import preprocess, create_context_target, convert_one_hot
+from w2vec.CBow import CustomCBOW
+from w2vec.SkipGram import CustomSkipGram
+import pickle # for save
 
-
-def getRandomContext(corpus, C=5):
-    wordID = random.randint(0, len(corpus) - 1)
-    
-    context = corpus[max(0, wordID - C):wordID]
-    if wordID+1 < len(corpus):
-        context += corpus[wordID+1:min(len(corpus), wordID + C + 1)]
-
-    centerword = corpus[wordID]
-    context = [w for w in context if w != centerword]
-
-    if len(context) > 0:
-        return centerword, context
+def word2vec_trainer(corpus, word2ind, mode="CBOW", dimension=64, learning_rate=0.01, iteration=50000, batch_size=500, window_size=3):
+    vocab_size = len(word2ind)
+    contexts, target = create_context_target(corpus, window_size)
+    target = convert_one_hot(target, vocab_size).float()
+    contexts = convert_one_hot(contexts, vocab_size).float()
+    batch_size = min(batch_size, len(target))
+    optimizer = SGD(lr=learning_rate)
+    losses = []
+    parallel_num = contexts.shape[1]
+    print("Number of words : %d" % (len(target)))
+    #################### model initialization ####################
+    if mode == "CBOW":
+        model = CustomCBOW(vocab_size, dimension, vocab_size, parallel_num)
+    elif mode == "SG":
+        model = CustomSkipGram(vocab_size, dimension, vocab_size, parallel_num)
     else:
-        return getRandomContext(corpus, C)
-
-
-def Skipgram(center, context, inputMatrix, outputMatrix):
-################################  Input  ################################
-# center : Index of a centerword (type:int)                             #
-# context : Index of a contextword (type:int)                           #
-# inputMatrix : Weight matrix of input (type:torch.tesnor(V,D))         #
-# outputMatrix : Weight matrix of output (type:torch.tesnor(V,D))       #
-#########################################################################
-
-###############################  Output  ################################
-# loss : Loss value (type:torch.tensor(1))                              #
-# grad_emb : Gradient of word vector (type:torch.tensor(1,D))           #
-# grad_out : Gradient of outputMatrix (type:torch.tesnor(V,D))          #
-#########################################################################
-
-    loss = None
-    grad_emb = None
-    grad_out = None
-
-    return loss, grad_emb, grad_out
-
-def CBOW(center, context, inputMatrix, outputMatrix):
-################################  Input  ################################
-# center : Index of a centerword (type:int)                             #
-# context : Indices of contextwords (type:list(int))                    #
-# inputMatrix : Weight matrix of input (type:torch.tesnor(V,D))         #
-# outputMatrix : Weight matrix of output (type:torch.tesnor(V,D))       #
-#########################################################################
-
-###############################  Output  ################################
-# loss : Loss value (type:torch.tensor(1))                              #
-# grad_emb : Gradient of word embedding (type:torch.tensor(1,D))        #
-# grad_out : Gradient of outputMatrix (type:torch.tesnor(V,D))          #
-#########################################################################
-
-    loss = None
-    grad_emb = None
-    grad_out = None
-
-    return loss, grad_emb, grad_out
-
-
-def word2vec_trainer(corpus, word2ind, mode="CBOW", dimension=64, learning_rate=0.05, iteration=50000):
-
-	#initialization
-    W_emb = torch.randn(len(word2ind), dimension) / (dimension**0.5)
-    W_out = torch.randn(len(word2ind), dimension) / (dimension**0.5)
-    window_size = 5
-
-    losses=[]
+        print("Unkwnown mode : "+mode)
+        exit()
+    ##############################################################
     for i in range(iteration):
-        #Training word2vec using SGD
-        centerWord, contextWords = getRandomContext(corpus, window_size)
-        centerInd = None
-        contextInds = None
-        
-        #learning rate decay
+        ################## getRandomContext ##################
+        index = torch.randperm(len(target))[0:batch_size]
+        centerWord, contextWords = target[index], contexts[index]
+        ################## learning ##################    
+        loss = model.forward(contextWords, centerWord)
+        model.backward()
+        optimizer.update(model)
+        W_emb = model.get_inputw()
+        W_out = model.get_outputw()
+        losses.append(loss)
+        ################## learning rate decay ##################
         lr = learning_rate*(1-i/iteration)
-
-        if mode=="CBOW":
-            L, G_emb, G_out = CBOW(centerInd, contextInds, W_emb, W_out)
-            W_emb[contextInds] -= lr*G_emb
-            W_out -= lr*G_out
-            losses.append(L.item())
-
-        elif mode=="SG":
-        	for contextInd in contextInds:
-                L, G_emb, G_out = Skipgram(centerInd, contextInd, W_emb, W_out)
-                W_emb[centerInd] -= lr*G_emb.squeeze()
-                W_out -= lr*G_out
-                losses.append(L.item())
-        else:
-            print("Unkwnown mode : "+mode)
-            exit()
-
-        if i%10000==0:
+        optimizer.set_lr(lr)
+        #########################################################
+        if i%1000==0:
         	avg_loss=sum(losses)/len(losses)
         	print("Loss : %f" %(avg_loss,))
         	losses=[]
@@ -103,8 +53,27 @@ def word2vec_trainer(corpus, word2ind, mode="CBOW", dimension=64, learning_rate=
     return W_emb, W_out
 
 def main():
+    with open('text8', 'r') as f:
+        text = f.read()
 	# Write your code of data processing, training, and evaluation
 	# Full training takes very long time. We recommend using a subset of text8 when you debug
-	pass
+    corpus, word2ind, _ = preprocess(text, subset=1e-4)
+    print("processing completed")
+    W_emb, W_out = word2vec_trainer(corpus, word2ind, mode="SG", learning_rate=0.01, iteration=50000, window_size=1)
+    
+    # plot (not sure, skipgram 보고 나중에 수정할게요)
+    # trainer.plot()
 
+    # saved
+    # params = {}
+    # params['word_vecs'] = W_emb.astype(np.float16)
+    # params['word_out'] = W_out.astype(np.float16)
+    # if mode == 'CBOW':
+    #     pkl_file = 'cbow_params.pkl'
+    # elif mode == 'SG':
+    #     pkl_file = 'skipgram_params.pkl'
+
+    # with open(pkl_file, 'wb') as f:
+    #     pickle.dump(params, f, -1)
+    
 main()
