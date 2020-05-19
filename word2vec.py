3cf54dc790960389de153102f3628ec2540326f5
unknown <sodlqnf123@naver.com>
2020-05-19 19:29:25 +0900

    slice data

--- word2vec.py
+++ word2vec.py
@@ -13,43 +13,57 @@ from common.util import most_similar_byEmb
 
 def word2vec_trainer(corpus, word2ind, mode="CBOW", dimension=64, learning_rate=0.01, iteration=50000, batch_size=500, window_size=3):
     vocab_size = len(word2ind)
-    contexts, target = create_context_target(corpus, window_size)
-    target = convert_one_hot(target, vocab_size).float()
-    contexts = convert_one_hot(contexts, vocab_size).float()
-    batch_size = min(batch_size, len(target))
-    optimizer = SGD(lr=learning_rate)
     losses = []
-    parallel_num = contexts.shape[1]
-    print("Number of words : %d" % (len(target)))
+    sum_iter = 0
+    parallel_num = 2 * window_size # number of parallel affine layers
+    slice_len = 10000
     #################### model initialization ####################
     if mode == "CBOW":
         model = CustomCBOW(vocab_size, dimension, vocab_size, parallel_num)
     elif mode == "SG":
         model = CustomSkipGram(vocab_size, dimension, vocab_size, parallel_num)
     else:
-        print("Unkwnown mode : "+mode)
+        print("Unkwnown mode : " + mode)
         exit()
-    ##############################################################
-    for i in range(iteration+1):
-        ################## getRandomContext ##################
-        index = torch.randperm(len(target))[0:batch_size]
-        centerWord, contextWords = target[index], contexts[index]
-        ################## learning ##################    
-        loss = model.forward(contextWords, centerWord)
-        model.backward()
-        optimizer.update(model)
-        W_emb = model.get_inputw()[0] # exclude bias
-        W_out = model.get_outputw()[0] # exclude bias
-        losses.append(loss)
-        ################## learning rate decay ##################
-        lr = learning_rate*(1-i/iteration)
-        optimizer.set_lr(lr)
-        #########################################################
-        if i%1000==0:
-        	avg_loss=sum(losses)/len(losses)
-        	print("Iteration : %d / Loss : %f" %(i, avg_loss))
-        	losses=[]
-
+    optimizer = SGD(lr=learning_rate)
+    ######## corpus slicing (to avoid memory excess error) ########
+    corpus_sliced = []
+    head = 0
+    tail = 0
+    while tail < len(corpus):
+        tail = min(tail + slice_len, len(corpus))
+        corpus_sliced.append(corpus[max(0, head - 2 * window_size) : tail])
+        head = tail
+    print("Length of one slice : %d / Number of slices : %d" % (slice_len, len(corpus_sliced)))
+    ######################## Learning ########################
+    for i, sub_corpus in enumerate(corpus_sliced):
+        print("Learning for corpus slice #%d" % (i+1))
+        contexts, target = create_context_target(sub_corpus, window_size)
+        target = convert_one_hot(target, vocab_size).float()
+        contexts = convert_one_hot(contexts, vocab_size).float()
+        batch_size = min(batch_size, len(target))
+        for i in range(iteration+1):
+            sum_iter += 1
+            ################## getRandomContext ##################
+            index = torch.randperm(len(target))[0:batch_size]
+            centerWord, contextWords = target[index], contexts[index]
+            ################## learning ##################    
+            loss = model.forward(contextWords, centerWord)
+            model.backward()
+            optimizer.update(model)
+            losses.append(loss)
+            ################## learning rate decay ##################
+            lr = learning_rate*(1-i/iteration)
+            optimizer.set_lr(lr)
+            #########################################################
+            if sum_iter%1000==0:
+                avg_loss=sum(losses)/len(losses)
+                print("Iteration : %d / Loss : %f" %(sum_iter, avg_loss))
+    ######### Extract W matrix #########
+    W_in, b_in = model.get_inputw()
+    W_out, b_out = model.get_outputw()
+    W_emb = W_in
+    ####################################
     return W_emb, W_out
 
 def main():
@@ -58,9 +72,9 @@ def main():
         text = f.read()
 	# Write your code of data processing, training, and evaluation
 	# Full training takes very long time. We recommend using a subset of text8 when you debug
-    corpus, word2id, id2word = preprocess(text, subset=1e-5)
-    print("processing completed")
-    W_emb, W_out = word2vec_trainer(corpus, word2id, mode=mode, learning_rate=0.01, iteration=5000, window_size=1)
+    corpus, word2id, id2word = preprocess(text, subset=1e-2)
+    print("Processing completed")
+    W_emb, W_out = word2vec_trainer(corpus, word2id, mode=mode, learning_rate=0.01, iteration=5000, window_size=3)
 
     # saved
     params = {}
