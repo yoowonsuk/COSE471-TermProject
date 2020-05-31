c18e810d1aef7e3b37c78f1c4eccbc908a0189a7
yoowonsuk <yoowonsuk@github.com>
2020-06-01 05:18:47 +0900

    duplicaton remove

--- common/Negativesampling.py
+++ common/Negativesampling.py
@@ -5,7 +5,29 @@ from common.np import *  # import numpy as np
 from common.layers import Embedding, SigmoidWithLoss
 import collections
 
-# 
+class EmbeddingDot:
+    def __init__(self, W):
+        self.embed = Embedding(W)
+        self.params = self.embed.params
+        self.grads = self.embed.grads
+        self.cache = None
+
+    def forward(self, h, idx):
+        target_W = self.embed.forward(idx)
+        out = np.sum(target_W * h, axis=1)
+
+        self.cache = (h, target_W)
+        return out
+
+    def backward(self, dout):
+        h, target_W = self.cache
+        dout = dout.reshape(dout.shape[0], 1)
+
+        dtarget_W = dout * h
+        self.embed.backward(dtarget_W)
+        dh = dout * target_W
+        return dh
+
 class UnigramSampler:
     def __init__(self, corpus, power, sample_size):
         self.sample_size = sample_size
